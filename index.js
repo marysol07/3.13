5efbd8c8e0c069a527222acd81929089108de224
marysol07 <h5nk@fresnou.org>
2022-05-05 13:37:33 -0700

    inital commit

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 console.log('i happened')
+
+const cardList = document.querySelector( '.cardList');
+
+addCard( 'test');
+
+function addCard(value){
+    let card = document.createElement('div');
+    card.classList.add('card');
+    card.classList.add('active');
+    card.innerHTML = value;
+    cardList.appendChild(card);
+}
